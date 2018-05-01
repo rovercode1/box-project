5f3619284572c59d0d164b44e431792aa1034f51
Hayden Worden <haydenworden@outlook.com>
2018-05-01 18:30:48 +0100

    Basic javascript

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var inner = document.querySelectorAll(".inner");
 
 for(i = 0; i < inner.length;i++){
   inner[i].addEventListener("click", function(){
-    for(i = 0; i < inner.length +1;i++){
+    for(i = 0; i < inner.length +1 ;i++){
       inner[i].style.background="red"
       this.style.background="blue"
     }
