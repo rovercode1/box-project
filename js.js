eeb641460e0b47d4fe3d1a18f9b8d2d05e48f4a5
Hayden Worden <haydenworden@outlook.com>
2018-05-01 20:18:16 +0100

    Boxes setup

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-var inner = document.querySelectorAll(".inner");
+var inner = document.querySelectorAll(".in");
 
 for(i = 0; i < inner.length;i++){
   inner[i].addEventListener("click", function(){
     for(i = 0; i < inner.length +1 ;i++){
-      inner[i].style.background="red"
+      inner[i].style.background="rgba(156, 156, 156, 0.089)"
       this.style.background="blue"
     }
   })
