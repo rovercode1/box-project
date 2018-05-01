a9575bd8dbb2055e9a768c398fb52bee7269bd07
Hayden Worden <haydenworden@outlook.com>
2018-05-01 20:44:22 +0100

    Background images

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 var inner = document.querySelectorAll(".in");
-
+var colours = ["rgba(156, 156, 156, 0.089)", "rgba(156, 156, 156, 0.089)","rgba(156, 156, 156, 0.089)",]
 for(i = 0; i < inner.length;i++){
   inner[i].addEventListener("click", function(){
     for(i = 0; i < inner.length +1 ;i++){
-      inner[i].style.background="rgba(156, 156, 156, 0.089)"
-      this.style.background="blue"
+      inner[i].style.transition="0.2s"
+      inner[i].style.background="red";
+      inner[i].style.opacity="0.5"
+      this.style.background="rgb(156, 156, 156";
     }
   })
 }
