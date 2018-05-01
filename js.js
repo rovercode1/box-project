36c359f80e45ecf620a195750841b2fb0c10982b
Hayden Worden <haydenworden@outlook.com>
2018-05-01 21:08:40 +0100

    Fixed

--- js.js
+++ js.js
@@ -4,9 +4,8 @@ for(i = 0; i < inner.length;i++){
   inner[i].addEventListener("click", function(){
     for(i = 0; i < inner.length +1 ;i++){
       inner[i].style.transition="0.2s"
-      inner[i].style.background="red";
-      inner[i].style.opacity="0.5"
-      this.style.background="rgb(156, 156, 156";
+      inner[i].style.background="rgba(255, 0, 0, 0.356)";
+      this.style.background="rgba(156, 156, 156, 0.356)";
     }
   })
 }
