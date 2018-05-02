08d1059cb6f6b43f15581561519f5e0c82441e07
Hayden Worden <haydenworden@outlook.com>
2018-05-02 07:02:32 +0100

    Fixed errors

--- js.js
+++ js.js
@@ -1,11 +1,20 @@
 var inner = document.querySelectorAll(".in");
 var colours = ["rgba(156, 156, 156, 0.089)", "rgba(156, 156, 156, 0.089)","rgba(156, 156, 156, 0.089)",]
+var bg = document.querySelector(".contain");
+
 for(i = 0; i < inner.length;i++){
   inner[i].addEventListener("click", function(){
-    for(i = 0; i < inner.length +1 ;i++){
+    for(i = 0; i < inner.length ;i++){
+      this.style.background="rgba(156, 156, 156, 0.356)";
       inner[i].style.transition="0.2s"
       inner[i].style.background="rgba(255, 0, 0, 0.356)";
-      this.style.background="rgba(156, 156, 156, 0.356)";
+
     }
   })
+}
+
+if(1 < 2){
+  for(var i = 0; i < 2;i++){
+    console.log("Hello!" + i)
+  }
 }
