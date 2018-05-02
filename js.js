c4ea2c801d4fc95366a96bb2763037be1e38b4f6
Hayden Worden <haydenworden@outlook.com>
2018-05-02 11:54:01 +0100

    bg image changes

--- js.js
+++ js.js
@@ -9,16 +9,24 @@ for(i = 0; i < inner.length;i++){
       inner[i].style.transition="0.2s"
       inner[i].style.background="rgba(255, 0, 0, 0.356)";
       if (this.id === "a") { 
-      this.style.background="green";
+      this.style.background="rgba(195, 100, 144, 0.356)";
+      bg.style.backgroundImage="url(img/left-black.jpeg)"
+      bg.style.backgroundSize="cover"; 
       }
       else if (this.id === "b") { 
-      this.style.background="pink";
+      this.style.background="rgba(130, 160, 72, 0.356)";
+       bg.style.background="url(img/left-woman.jpeg)"
+       bg.style.backgroundSize="cover"; 
       }
       else if (this.id === "c") { 
-      this.style.background="red";
+      this.style.background="rgba(71, 158, 142, 0.356)";
+       bg.style.background="url(img/right-black.jpeg)"
+       bg.style.backgroundSize="cover"; 
       }
       else if (this.id === "d") { 
-      this.style.background="black";
+      this.style.background="rgba(241, 137, 76, 0.356)";
+       bg.style.background="url(img/right-white.jpeg)"
+       bg.style.backgroundSize="cover"; 
       }
     }
   })
